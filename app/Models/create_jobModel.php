9728296b4241d2a9ede746a873a4f9a11f5f6d7d
loganjp31 <logan.jonespatterson@ontariotechu.net>
2025-11-15 16:24:08 -0500

    added the create job functionality in job model.

--- app/Models/create_jobModel.php
+++ app/Models/create_jobModel.php
@@ -1,1 +1,27 @@
 <?php
+
+require_once __DIR__ . '/../../config.php';
+
+function createJob($title, $description, $location, $salary, $companyId, $jobType)
+{
+    global $pdo;
+
+    $sql = "INSERT INTO jobs (title, desc, location, pay, orgRSN, job_type)
+            VALUES (:title, :description, :location, :salary, :orgRSN, :job_type`);";
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':description', $description);
+    $stmt->bindParam(':location', $location);
+    $stmt->bindParam(':salary', $salary);
+    $stmt->bindParam(':orgRSN', $companyId);
+    $stmt->bindParam(':job_type', $jobType);
+
+    try {
+        $stmt->execute();
+        return $pdo->lastInsertId();
+    } catch (PDOException $e) {
+        error_log("Database error: " . $e->getMessage());
+        return null;
+    }
+}
