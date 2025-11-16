6ac401f98bd8c3f7237c50cb3f3af99abcb5e0f5
loganjp31 <logan.jonespatterson@ontariotechu.net>
2025-11-15 19:09:27 -0500

    added sql search for tagModel

--- app/Models/tagModel.php
+++ app/Models/tagModel.php
@@ -1,9 +1,22 @@
 <?php
+
+require_once __DIR__ . '/../../config.php';
+
 class TagModel {
     private $tags;
 
     public function __construct() {
+
+        global $pdo;
+
+        $sql = "SELECT name FROM skills GROUP BY name ORDER BY COUNT(*) DESC;";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $this->tags = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+
         // Hardcoded tags for now; later can fetch from DB
+        /*
         $this->tags = [
             "JavaScript",
             "Python",
@@ -13,6 +26,7 @@ class TagModel {
             "React",
             "Node.js"
         ];
+        */
     }
 
     public function getTags() {
