d670fa4f86887f92de7a9d6a3b3bb367a8435daa
Steven-Sunny <sunsteven796@gmail.com>
2025-11-15 20:01:01 -0500

    Added functions to job-detailsModel.php

--- app/Models/job-detailsModel.php
+++ app/Models/job-detailsModel.php
@@ -1,6 +1,37 @@
 <?php
 class jobDetailsModel{
     public static function getJobDetailsById($jobId){
-        
+        global $pdo;
+        $sql = "SELECT j.title, j.description, j.location, j.pay, j.location, j.pay, j.hours, s.skill, s.skillgroup
+                FROM jobs j
+                JOIN skills_want w ON j.job_id = w.job_id
+                JOIN skills s ON s.skill_id = w.skill_id
+                WHERE j.jobRSN = :jobId";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':jobId', $jobId);
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($result){
+            return $result;
+        }else{
+            return null;
+        }
+    }
+    public static function getOrgIdByJobId($jobId){
+        global $pdo;
+        $sql = "SELECT j.orgRSN
+                FROM jobs j
+                WHERE j.jobRSN = :jobId";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':jobId', $jobId);
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($result){
+            return $result;
+        }else{
+            return null;
+        }
     }
 }
