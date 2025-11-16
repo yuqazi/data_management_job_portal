1b7eeda77fc39209e081368c5fde10c205a17577
Steven-Sunny <sunsteven796@gmail.com>
2025-11-15 20:02:00 -0500

    Added functions to job-detailsModel.php

--- app/Models/job-detailsModel.php
+++ app/Models/job-detailsModel.php
@@ -4,8 +4,8 @@ class jobDetailsModel{
         global $pdo;
         $sql = "SELECT j.title, j.description, j.location, j.pay, j.location, j.pay, j.hours, s.skill, s.skillgroup
                 FROM jobs j
-                JOIN skills_want w ON j.job_id = w.job_id
-                JOIN skills s ON s.skill_id = w.skill_id
+                JOIN skills_want w ON j.jobRSN = w.jobRSN
+                JOIN skills s ON s.skillRSN = w.skillRSN
                 WHERE j.jobRSN = :jobId";
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':jobId', $jobId);
